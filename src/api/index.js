3427a1ef11b8ef07b6a1cec8ebd4e3f2ca605a34
VickyBondarenko <quikyv@gmail.com>
2023-12-06 18:09:22 +0100

    change api key

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiKey = '3d474b4b143267599ff69bdae2b1bdff';
+const apiKey = 'db45881e2afb9e1808a4f023bca65e3c';
 export const getTrendingMovies = () => {
   return axios.get(
     `https://api.themoviedb.org/3/trending/movie/day?api_key=${apiKey}`
